a0c85b48fb1467e9c3cefcded9faaa62076f1a82
hampus <Coyote@DESKTOP-17273MG>
2017-12-14 09:31:12 +0100

    done

--- pancakes.php
+++ pancakes.php
@@ -2,7 +2,6 @@
 include 'resources/views/fragments/init.php';
 $_SESSION['currentpage'] = 'pancakes';
 
-include 'resources/httpParamVarHandlers/pancakesVars.php';
 
 include 'resources/views/pancakespage.php';
 $contr->shutdown();
