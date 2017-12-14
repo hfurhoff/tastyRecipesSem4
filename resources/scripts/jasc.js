8dccb79baaa6173bbb682df85f23a740990c82a9
hampus <Coyote@DESKTOP-17273MG>
2017-12-14 09:28:04 +0100

    done

--- resources/scripts/jasc.js
+++ resources/scripts/jasc.js
@@ -1,15 +1,21 @@
-$(document).ready(function(){    
+$(document).ready(function(){
     
-    var commentToAdd = new CommentToAdd();
-    ko.applyBindings(commentToAdd, document.getElementById('commentformdiv'));
-    ko.applyBindings(new Comments(commentToAdd), document.getElementById('comments'));
+    function CommentToAdd(){
+        var self = this;
+        self.username = $('#anvnamn').text();
+        self.newComment = ko.observable("");
+        self.submitComment = function(){
+            if (self.newcomment !== ""){
+                $.post("directinput.php", "newComment=" + ko.toJS(self.newComment));
+                self.newComment("");
+            }
+        };
+    }
     
     function Comments(commentToAdd){
         var self = this;
         self.commentToAdd = commentToAdd;
         self.commentsMade = ko.observableArray();
-        
-        self.getNewComments(-1);
         
         self.getNewComments = function(no){
             $.getJSON("getComments.php", "noOfComments=" + no, function (jsonComments){
@@ -37,21 +43,15 @@ $(document).ready(function(){
             self.commentsMade.remove(comment);
             $.post("directinput.php", "timestamp=" + ko.toJS(comment.timestamp));
         };
+        
+        self.getNewComments(-1);
     }
     
     function removeQuotes(str) {
         return str.replace(/\"/g, "");
     }
     
-    function CommentToAdd(){
-        var self = this;
-        self.username = $('#anvnamn').text();
-        self.newComment = ko.observable("");
-        self.submitComment = function(){
-            if (self.newcomment !== ""){
-                $.post("directinput.php", "newComment=" + ko.toJS(self.newComment));
-                self.newComment("");
-            }
-        };
-    }
+    var commentToAdd = new CommentToAdd();
+    ko.applyBindings(commentToAdd, document.getElementById('commentformdiv'));
+    ko.applyBindings(new Comments(commentToAdd), document.getElementById('comments'));
 })
